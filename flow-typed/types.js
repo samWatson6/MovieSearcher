e5bf5f2c5058cc3617cba68afaeb73a68268c3e3
Sam Watson <Sam.Watson3775@gmail.com>
2018-09-18 13:11:43 -0700

    Create types for actions

--- flow-typed/types.js
+++ flow-typed/types.js
@@ -23,11 +23,13 @@ export type State = {
   }
 };
 
-declare type ActionType = 'SET_SEARCH_TERM' | 'ADD_API_DATA';
+declare type ActionType = "SET_SEARCH_TERM" | "ADD_API_DATA";
 
 declare type ActionT<A: ActionType, P> = {|
   type: A,
   payload: P
 |};
 
-export type Action = ActionT<'SET_SEARCH_TERM', string> | ActionT<'ADD_API_DATA', Show>;
+export type Action =
+  | ActionT<"SET_SEARCH_TERM", string>
+  | ActionT<"ADD_API_DATA", Show>;
