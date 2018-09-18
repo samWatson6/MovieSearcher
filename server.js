f4b7af04e0b5bfe71fba233c5b21b46d341d3c64
Sam Watson <Sam.Watson3775@gmail.com>
2018-09-18 13:10:29 -0700

    Add server side rendering with compression

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 /* eslint no-console:0 */
-require('babel-register');
+require("babel-register");
 
-const express = require('express');
-const React = require('react');
-const ReactDOMServer = require('react-dom/server');
-const ReactRouter = require('react-router-dom');
-const _ = require('lodash');
-const fs = require('fs');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-const webpackHotMiddleware = require('webpack-hot-middleware');
-const compression = require('compression');
-const webpack = require('webpack');
-const config = require('./webpack.config');
-const App = require('./js/App').default;
+const express = require("express");
+const React = require("react");
+const ReactDOMServer = require("react-dom/server");
+const ReactRouter = require("react-router-dom");
+const _ = require("lodash");
+const fs = require("fs");
+const webpackDevMiddleware = require("webpack-dev-middleware");
+const webpackHotMiddleware = require("webpack-hot-middleware");
+const compression = require("compression");
+const webpack = require("webpack");
+const config = require("./webpack.config");
+const App = require("./js/App").default;
 
 const StaticRouter = ReactRouter.StaticRouter;
 const port = 8080;
-const baseTemplate = fs.readFileSync('./index.html');
+const baseTemplate = fs.readFileSync("./index.html");
 const template = _.template(baseTemplate);
 
 const server = express();
 server.use(compression());
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   const compiler = webpack(config);
   server.use(
     webpackDevMiddleware(compiler, {
@@ -30,12 +30,16 @@ if (process.env.NODE_ENV === 'development') {
   );
   server.use(webpackHotMiddleware(compiler));
 }
-server.use('/public', express.static('./public'));
+server.use("/public", express.static("./public"));
 
 server.use((req, res) => {
   const context = {};
   const body = ReactDOMServer.renderToString(
-    React.createElement(StaticRouter, { location: req.url, context }, React.createElement(App))
+    React.createElement(
+      StaticRouter,
+      { location: req.url, context },
+      React.createElement(App)
+    )
   );
 
   if (context.url) {
